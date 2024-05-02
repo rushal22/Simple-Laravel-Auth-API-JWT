c3c737c45310d1d4a15ab8afe350d0a2014d3070
Sujal Maharjan <sujalmaharjan2059@gmail.com>
2024-05-02 16:16:37 +0545

    Initial commit

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,104 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Validator;
+
+class UserController extends Controller
+{
+    public function register(Request $request){
+        $data = $request->all();
+        $validator = Validator::make($request->all(),[
+            'name'=>'required|max:50',
+            'email'=>'required|email|unique:users',
+            'password'=>'required|min:8|confirmed'
+        ]);
+        if($validator){
+            $user = new User;
+            $user->name = $data['name'];
+            $user->email = $data['email'];
+            $user->password = Hash::make($data['password']);
+            $user->save();
+            return response()->json(['message'=>'successfull', 'data'=>$user], 200);
+        }
+        else{
+            return response()->json(['message'=>'error occurred', 'error'=> $validator->errors()], 400);
+        }
+    }
+
+    public function login(Request $request){
+        $data = $request->all();
+
+        $validator = Validator::make($request->all(),[
+            'email'=>'required',
+            'password'=>'required'
+        ]);
+        
+        if($validator->fails()){
+            return response()->json(['message'=>'error occurred'],400);
+        }
+        else{
+            if(Auth::attempt($data)){
+                $loginEmail = [
+                        'email'=>$data['email'],
+                        'password'=>$data['password']
+                    ];
+                $token = Auth::attempt($loginEmail);
+                
+                $response = $this->respondWithToken($token);
+                return response()->json(['message'=>'successfull', 'token'=>$response],200);
+            }
+            else{
+                return response()->json(['message'=>'invalid credentials'], 400);
+            }
+            // $loginEmail = [
+                //     'email'=>$data['email'],
+                //     'password'=>$data['password']
+                // ];
+
+            // $loggedin = false;
+            // $token = '';
+            // if($log2 = Auth::attempt($loginEmail)){
+            //     $loggedin = true;
+            //     $token = $log2;
+            // }
+
+            // if($loggedin == false){
+            //     return response()->json(['message'=>'invalid'], 400);
+            // }
+
+            // $response = $this->respondWithToken($token);
+            // return response()->json(['token'=>$response],200);
+
+            
+        }
+    }
+
+    public function respondWithToken($token){
+        return response()->json([
+            'message'=>'logged in successfully', 
+            'token_type'=>'Bearer', 
+            'access_token'=>$token
+        ]);
+    }
+
+    public function profile(Request $request){
+        return response()->json(auth()->user());
+    }
+
+    public function logout(){
+        $log = Auth::guard('api')->logout();
+
+        if($log){
+            
+            return response()->json(['message'=>'logged out successfully'], 200);
+        }
+        else{
+            return response()->json(['message'=>'failed'], 400);
+        }
+    }
+}
---
+++ routes/api.php
@@ -0,0 +1,22 @@
+<?php
+
+use GuzzleHttp\Middleware;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\UserController;
+
+// Route::get('/user', function (Request $request) {
+//     return $request->user();
+// })->middleware('auth:sanctum');
+
+Route::group(['middleware'=>'api'], function($routes){
+    
+    Route::post('register', [UserController::class, 'register']);
+
+    Route::post('login', [UserController::class, 'login']);
+
+    Route::get('profile', [UserController::class, 'profile']);
+
+    Route::post('logout', [UserController::class, 'logout']);
+
+});
